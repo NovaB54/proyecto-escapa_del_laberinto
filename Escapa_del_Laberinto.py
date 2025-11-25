aa6366f3a346ff746d15f38e97db462d6e54bfaf
Isaac Arce <emmarce.13@gmail.com>
2025-11-24 18:26:46 -0600

    Cambios en el archivo Esacapa_del_Laberinto

--- Escapa_del_Laberinto.py
+++ Escapa_del_Laberinto.py
@@ -1,1 +1,14 @@
 #Desarrollo del juego principal
+#
+#
+#Clase Terreno para hacer variedad de casillas
+class Terreno:
+    def __init__(self, fila, columna):
+        self.fila = fila
+        self.columna = columna
+
+    def permiteJugador(self):
+        return False
+
+    def permiteCazador(self):
+        return False
