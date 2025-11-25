465f651df7b461c2baf3c2cdcb13b654790787de
Isaac Arce <emmarce.13@gmail.com>
2025-11-24 18:32:26 -0600

    Implementacion de las clases hijos de Terreno

--- Escapa_del_Laberinto.py
+++ Escapa_del_Laberinto.py
@@ -1,6 +1,9 @@
 #Desarrollo del juego principal
 #
 #
+import pygame
+#
+#
 #Clase Terreno para hacer variedad de casillas
 class Terreno:
     def __init__(self, fila, columna):
@@ -10,5 +13,38 @@ class Terreno:
     def permiteJugador(self):
         return False
 
+    def permiteCazador(self):
+        return False
+    
+#hijos de Terrenos (Todas las casillas que van a existir)
+
+class Camino(Terreno):
+    def permiteJugador(self):
+        return True
+    
+    def permiteCazador(self):
+        return True
+
+
+class Muro(Terreno):
+    def permiteJugador(self):
+        return False
+    
+    def permiteCazador(self):
+        return False
+
+
+class Liana(Terreno):
+    def permiteJugador(self):
+        return False
+    
+    def permiteCazador(self):
+        return True
+
+
+class Tunel(Terreno):
+    def permiteJugador(self):
+        return True
+    
     def permiteCazador(self):
         return False
